ccaa8d58004e7bb4036d520556e8808f53cc8296
IOANNA <115180674+iwannaefraimia@users.noreply.github.com>
2025-03-23 19:41:37 +0200

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,22 +2,18 @@ import pytest
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from app import app, db  # Εισάγετε το app και db από το αρχείο app.py
-
+from app import app, db  
 @pytest.fixture
 def init_db():
-    # Δημιουργία application context
     with app.app_context():
-        db.create_all()  # Δημιουργία όλων των tables στη βάση δεδομένων
-    yield db  # Επιστροφή της βάσης δεδομένων για χρήση στα tests
+        db.create_all()  
+    yield db  
     with app.app_context():
-        db.drop_all()  # Καθαρισμός των tables μετά τη δοκιμή
+        db.drop_all()  
 
 def test_create_resource(init_db):
-    # Η δοκιμή για τη δημιουργία πόρων
     pass
 
 def test_get_resources(init_db):
-    # Η δοκιμή για να πάρετε πόρους
     pass
 
